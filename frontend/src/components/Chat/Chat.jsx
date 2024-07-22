7497f56ab96adad9c40e31ef27e6aa41ceb37969
Mikhail <dukin.misha22@gmail.com>
2024-07-22 23:46:56 +0300

    test

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -55,7 +55,7 @@ const Chat = () => {
   }, [messages]);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5001");
+    const newSocket = io();
     setSocket(newSocket);
     return () => {
       newSocket.disconnect();
